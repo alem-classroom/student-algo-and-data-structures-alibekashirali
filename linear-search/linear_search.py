448c57641907425bd3cc2755cfc6460f2e5e7ef1
Alibek Ashirali <ashiralialibek@gmail.com>
2021-07-20 21:14:21 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,9 +2,7 @@ def linear_search(lst, to_find):
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
-  for i in range(0, len(lst)-1):
+  for i in range(len(lst)):
     if lst[i] == to_find:
       return i
-    else:
-      return -1
-  
+    return -1
